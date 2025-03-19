b32d5c6d3a55e47cd8e81ba8f481ea4bd9de0d1b
Colombo <colombo.carlos@gmail.com>
2025-03-19 13:27:56 -0600

    Initializing proyect import constants

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print("Starting Asteroids!")
+#Here I start Asteroids game!
+
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
