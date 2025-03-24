5ea6ab63ece5fb150fb547a5ef603dbf9a235155
Colombo <colombo.carlos@gmail.com>
2025-03-24 17:54:57 -0600

    player moving finished!

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-#Here I start Asteroids game!
 import pygame
-from pygame.locals import *
 from constants import *
 from player import Player
 
@@ -10,21 +8,34 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable= pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
+    dt = 0
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
-        screen.fill((0, 0, 0))
-        player.draw(screen)
+        updatable.update(dt)
+
+        screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000.0
 
 
